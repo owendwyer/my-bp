3cea765af17764fe171ec83bbf3d198f38f1fb73
Owen Dwyer <odwyer@gmail.com>
2020-12-28 11:51:09 +0700

    bind for orientationChange

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ class Main {
 	preload() {
 		console.log('todo - need to bundle js src from the indextemplatedev.html and use that instead');
 		console.log('todo - setup polyfill');
+		this.orientationChange = this.orientationChange.bind(this);
 		this.preloader = new Preloader(this.preloadComplete.bind(this));
 		this.preloader.startLoad();
 	}
